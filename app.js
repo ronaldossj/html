b5331ba559c75fca3ffbe83f69bc0dcaa3d49d10
Ronaldo Serafim <ronaldo.serafim.96@outlook.com.br>
2022-10-24 10:52:33 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+'use strict'
+
+ function vermelho() {
+    ul.style.color = 'red'
+    h1.style.color = 'red'
+    msg.style.color = 'red'
+  }
+
+ function verde() {
+    ul.style.color = 'green'
+    h1.style.color = 'green'
+    msg.style.color = 'green'
+ }
+
+ function azul() {
+    ul.style.color = 'blue'
+    h1.style.color = 'blue'
+    msg.style.color = 'blue'
+ }
+
+ function preto() {
+    ul.style.color = 'black'
+    h1.style.color = 'black'
+    msg.style.color = 'black'
+ }
+
+ function branco() {
+    ul.style.color = 'white'
+    h1.style.color = 'white'
+    msg.style.color = 'white'
+ }
+
+ function vermelhoF() {
+    document.body.style.background = 'red'
+ }
+
+ function verdeF() {
+    document.body.style.background = 'green'
+ }
+
+ function azulF() {
+    document.body.style.background = 'blue'
+ }
+
+ function pretoF() {
+    document.body.style.background = 'black'
+ }
+
+ function brancoF() {
+    document.body.style.background = 'white'
+ }
